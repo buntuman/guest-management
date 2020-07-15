1d5849e3f7ce15342e23890e56aff863fc04f1cf
buntuman <danielgitahi5@gmail.com>
2020-07-14 20:53:12 -0400

    added db connection

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const guestRouter = require('./middlewares/guests');
+require('dotenv/config');
 
 app.use(express.json());
 app.use('/api/guest',guestRouter);
+
+// connect to database
+mongoose.connect(process.env.DB_CONNECTION,{useNewUrlParser: true,useUnifiedTopology: true },() => console.log('connected to db. Code is : '+mongoose.connection.readyState));
 
 app.listen(3000);
